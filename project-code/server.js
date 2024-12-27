317dc7d9de3d59d734b02994c763094e4f7c2a19
root <root@master.localdomain>
2024-12-28 00:21:33 +0100

    iiiiiiiiiiii

--- project-code/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-const port = 3000;
-
-const server = app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
-
-module.exports = server; // Export server for potential use elsewhere
-
